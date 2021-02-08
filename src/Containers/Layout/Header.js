27e7a9d8194993ecf225dc188562c303359506de
juanjose.ortiz <juanjose.ortiz@optimissa.com>
2021-02-08 22:17:21 +0100

    se añade un link al logo para ir al home page

--- src/Containers/Layout/Header.js
+++ src/Containers/Layout/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../Styles/layout.css'
+import { Link } from "wouter";
 import logo from '../../Styles/images/logo_ilerna.png'
 
 export default class Header extends React.Component{
@@ -14,7 +15,9 @@ constructor(props){
   render = () => (
     <div className='layout-header'>
         <div className='layout-header__bar'>
+        <Link href="/">
             <img src={logo} className='layout-logo'/>
+        </Link>
             <div className='layout-header__navbar'>
                 <div className='layout-header__navbar-item'>GALLERY</div>
                 <div className='layout-header__navbar-item'>HISTORIES</div>
